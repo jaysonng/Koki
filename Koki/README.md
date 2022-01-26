591ef472fb430ce48c6d150e50a9b6994460ba74
Jayson Ng <jayson.ng@gmail.com>
2022-01-26 11:14:23 +0800

    Created new XCode Swift Package Project
    Created new XCode Swift Package Project

---
+++ Koki/README.md
@@ -0,0 +1,3 @@
+# Koki
+
+A description of this package.
---
+++ Koki/Tests/KokiTests/KokiTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Koki
+
+final class KokiTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Koki().text, "Hello, World!")
+    }
+}
