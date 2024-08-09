6cfac62b33176e2d5a98c0cd0cbb0b91c4239144
Jayson Ng <jayson@jay.ph>
2024-08-09 14:46:38 +0800

    update

--- Sources/Koki/Koki.swift
+++ Sources/Koki/Koki.swift
@@ -7,11 +7,43 @@
 
 import Foundation
 
-public struct Koki {
-    public private(set) var text = "Hello, World!"
+typealias ViewState = KokiState
 
-    public init() {
-    }
+public struct Koki { }
+
+public protocol ViewLoadable {
+    var state: KokiState { get set }
 }
 
-
+public enum KokiState: String, CustomStringConvertible {
+    
+    case isIdle
+    case isLoading
+    case isLoaded
+    case errorLoading
+    case reloadView
+    
+    public var description: String {
+        switch self {
+        case .isIdle                : return "View is idle."
+        case .isLoading             : return "View is loading."
+        case .isLoaded              : return "View is done loading."
+        case .errorLoading          : return "Something went wrong loading the view."
+        case .reloadView            : return "Reloading the view..."
+        }
+    }
+    
+    public var isLoading: Bool {
+        get {
+            switch self {
+            case .isLoading, .isIdle:
+                return true
+            default:
+                return false
+            }
+        }
+        set {
+            self = newValue ? .isLoading : .isLoaded
+        }
+    }
+}
--- Sources/Koki/String+extensions.swift
+++ Sources/Koki/String+extensions.swift
@@ -8,58 +8,29 @@
 import Foundation
 import SwiftUI
 
-public protocol Loadable {
-    var state: KokiState { get set }
-}
+public extension String {
 
-typealias ViewState = KokiState
-
-public enum KokiState: String, CustomStringConvertible {
-    case isIdle
-    case isLoading
-    case isLoaded
-    case errorLoading
-    case reloadView
-
-    public var description: String {
-        switch self {
-        case .isIdle                : return "View is idle."
-        case .isLoading             : return "View is loading."
-        case .isLoaded              : return "View is done loading."
-        case .errorLoading          : return "Something went wrong loading the view."
-        case .reloadView            : return "Reloading the view..."
-        }
+    /// Checks if the given String is Numeric
+    ///
+    /// - Returns: Bool
+    var isNumeric: Bool {
+        guard self.count > 0 else { return false }
+        let nums: Set<Character> = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        return Set(self).isSubset(of: nums)
     }
-    
-    public var isLoading: Bool {
-        get {
-            switch self {
-            case .isLoading:
-                return true
-            default:
-                return false
-            }
-        }
-        set {
-            self = newValue ? .isLoading : .isLoaded
-        }
-    }
-}
 
-extension String {
-    
-    /// Checks if the given String is a valid e-mail address
-    /// - returns: Bool
+    /// Checks if the given String is a valid email address
+    ///
+    /// - Returns: Bool
     func isValidEmail() -> Bool {
         let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
         let emailPred = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
         return emailPred.evaluate(with: self)
     }
+
     
-    ///  Remove white spaces and new lines from String
-    /// - Returns: Removed white space and new lines
-    func trimmed() -> String {
-        self.trimmingCharacters(in: .whitespacesAndNewlines)
+    var lines: [String] {
+        self.components(separatedBy: .newlines)
     }
     
     /// Trim String
@@ -67,4 +38,24 @@ extension String {
         self = self.trimmed()
     }
 
+    ///  Remove white spaces and new lines.
+    ///
+    /// - Returns: Removed white space and new lines
+    func trimmed() -> String {
+        self.trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+
+
+}
+
+//: https://stackoverflow.com/a/60641131/1455606
+public extension Optional {
+    var logable: Any {
+        switch self {
+        case .none:
+            return "<nil>|⭕️"
+        case let .some(value):
+            return value
+        }
+    }
 }
